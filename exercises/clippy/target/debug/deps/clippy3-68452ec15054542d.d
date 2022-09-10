3a398ffcf836224a1465b27d3d8f3efa5cf80b13
MK <12462272+Valentin4869@users.noreply.github.com>
2022-09-10 15:41:35 +0300

    Add files via upload

---
+++ exercises/clippy/target/debug/deps/clippy3-68452ec15054542d.d
@@ -0,0 +1,7 @@
+C:\Users\Mir\rustlings\exercises\clippy\target\debug\deps\clippy3-68452ec15054542d.rmeta: clippy3.rs
+
+C:\Users\Mir\rustlings\exercises\clippy\target\debug\deps\clippy3-68452ec15054542d.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
