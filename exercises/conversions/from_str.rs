f2339b4e591e03930d5ef005889ccdfd6c827736
MK <12462272+Valentin4869@users.noreply.github.com>
2022-09-12 13:02:42 +0300

    Update from_str.rs

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -1,11 +1,3 @@
-// from_str.rs
-// This is similar to from_into.rs, but this time we'll implement `FromStr`
-// and return errors instead of falling back to a default value.
-// Additionally, upon implementing FromStr, you can use the `parse` method
-// on strings to generate an object of the implementor type.
-// You can read more about it at https://doc.rust-lang.org/std/str/trait.FromStr.html
-// Execute `rustlings hint from_str` or use the `hint` watch subcommand for a hint.
-
 use std::num::ParseIntError;
 use std::str::FromStr;
 
@@ -27,23 +19,6 @@ enum ParsePersonError {
     // Wrapped error from parse::<usize>()
     ParseInt(ParseIntError),
 }
-
-
-
-// Steps:
-// 1. If the length of the provided string is 0, an error should be returned
-// 2. Split the given string on the commas present in it
-// 3. Only 2 elements should be returned from the split, otherwise return an error
-// 4. Extract the first element from the split operation and use it as the name
-// 5. Extract the other element from the split operation and parse it into a `usize` as the age
-//    with something like `"4".parse::<usize>()`
-// 6. If while extracting the name and the age something goes wrong, an error should be returned
-// If everything goes well, then return a Result of a Person object
-//
-// As an aside: `Box<dyn Error>` implements `From<&'_ str>`. This means that if you want to return a
-// string error message, you can do so via just using return `Err("my error message".into())`.
-
-
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
@@ -51,18 +26,18 @@ impl FromStr for Person {
             return Err(ParsePersonError::Empty);
         }
 
-        let elements: Vec<_> = s.split(",").collect();
-        if elements.len() != 2 {
+        let e: Vec<_> = s.split(",").collect();
+        if e.len() != 2 {
             return Err(ParsePersonError::BadLen);
         }
 
-        let name = elements[0].to_string();
+        let name = e[0].to_string();
         if name.len() == 0 {
             return Err(ParsePersonError::NoName);
         }
 
-        let maybe_age = elements[1].parse::<usize>();
-        match maybe_age {
+        let ageq = e[1].parse::<usize>();
+        match ageq {
             Ok(age) => Ok(Person { name, age }),
             Err(e) => Err(ParsePersonError::ParseInt(e)),
         }
