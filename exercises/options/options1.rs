ed5922350ceab2f9dcb5b3ca60f117038b8d123c
Valentin4869 <12462272+Valentin4869@users.noreply.github.com>
2022-11-02 07:39:32 +0200

    Add files via upload

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -1,7 +1,7 @@
 // options1.rs
 // Execute `rustlings hint options1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 // This function returns how much icecream there is left in the fridge.
 // If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
@@ -13,7 +13,7 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     match time_of_day {
         x if x < 22 => Some(5),
         x if x <= 24 => Some(0),
-        _ => None
+        other => None,
     }
 }
 
